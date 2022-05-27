4778ac2e300f9d820e091c0dc8bf6866dfc0bcbc
anfoxy <61195842+anfoxy@users.noreply.github.com>
2022-05-27 18:32:56 +0300

    Update EmailConstants.java

--- src/main/java/com/netcracker/email/EmailConstants.java
+++ src/main/java/com/netcracker/email/EmailConstants.java
@@ -2,13 +2,10 @@ package com.netcracker.email;
 
 public class EmailConstants {
 
-    // Replace with your email here:
     public static final String MY_EMAIL = "test.java069@gmail.com";
 
-    // Replace password!!
     public static final String MY_PASSWORD = "Qwertyasd123";
 
-    // And receiver!
     public static final String FRIEND_EMAIL = "anfoxy27@gmail.com";
 
 }
